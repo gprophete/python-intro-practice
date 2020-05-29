70d5c34b66ce424afccac2095036ffe4992e4059
gprophete <garlyprophete30@gmail.com>
2020-05-29 10:56:25 -0400

    final game

--- lib/script.py
+++ lib/script.py
@@ -1,18 +1,43 @@
 from capitals import states
 import random
 
-print("Let's learn some capitals!!")
-random.shuffle(states)
+score = {
+    "correct":0,
+    "incorrect":0
+}
+
+print('''
+********************************
+*                               *
+*Let's learn some capitals!!    *
+*                               *
+********************************''')
+# random.shuffle(states)
 # print(states)
-for state in states:
-    question= "What is the capital of" + " " +state['name']
-    print(question)
-    answer= input()
-    # print (state['capital'])
-    if answer == state['capital']:
-        print("You got it")
-    else:
-        print("You ain't got it")
+while True:
+    random.shuffle(states)
+    for state in states:
+        question= "What is the capital of" + " " +state['name']
+        print(question)
+        answer= input()
+        # print (state['capital'])
+
+        if answer.lower() == state['capital'].lower():
+            print("You got it")
+            score['correct'] += 1
+        else:
+            print("You ain't got it")
+            score['incorrect'] += 1
+
+        right= score['correct']
+        wrong= score['incorrect']
+        print(f"you got {right} right")
+        print(f"you got {wrong} wrong")
+    
+    print("End game(y/n)")
+    choice = input()
+    if choice.lower() == 'n' or choice == '':
+        exit()
     
    
    
