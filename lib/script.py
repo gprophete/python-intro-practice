fd0d18cf4c50171349588692a99f490b3f5892c0
gprophete <garlyprophete30@gmail.com>
2020-05-28 22:23:55 -0400

    initial commit

---
+++ lib/script.py
@@ -0,0 +1,28 @@
+from capitals import states
+import random
+
+print("Let's learn some capitals!!")
+
+random.shuffle(states)
+# print(states)
+
+for state in states:
+#    print (state)
+
+   for keys, values in state.items():
+       print(keys, values)
+
+       
+
+
+        
+        
+
+        
+        
+        
+        
+
+
+
+    
