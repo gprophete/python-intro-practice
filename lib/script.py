48879576d88fae81a6bcf829b7b5391e58af6d10
gprophete <garlyprophete30@gmail.com>
2020-05-29 07:58:30 -0400

    printing question

--- lib/script.py
+++ lib/script.py
@@ -2,15 +2,20 @@ from capitals import states
 import random
 
 print("Let's learn some capitals!!")
-
 random.shuffle(states)
 # print(states)
-
 for state in states:
-#    print (state)
+    question= "What is the capital of" +" " +state['name']
+    answer= input()
+    print(question) 
+    
 
-   for keys, values in state.items():
-       print(keys, values)
+    
+   
+#    for keys, values in state.items():
+       
+
+
 
        
 
