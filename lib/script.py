3dd9afb223d44a55bde39010e1aa030c7d1f2c04
gprophete <garlyprophete30@gmail.com>
2020-05-29 08:26:05 -0400

    Answer input added with correct answer checked

--- lib/script.py
+++ lib/script.py
@@ -5,9 +5,20 @@ print("Let's learn some capitals!!")
 random.shuffle(states)
 # print(states)
 for state in states:
-    question= "What is the capital of" +" " +state['name']
+    question= "What is the capital of" + " " +state['name']
+    print(question)
     answer= input()
-    print(question) 
+    # print (state['capital'])
+    if answer == state['capital']:
+        print("You got it")
+    else:
+        print("You ain't got it")
+    
+   
+   
+
+    
+    
     
 
     
